e48e8819368d0860f6ddcf3fbba080c7231b18d1
valyanggy <yangx927@newschool.edu>
2021-03-27 00:03:09 +0800

    fixed previous issue
    fixed previous issue by rephrase if else statement in a proper way

--- main.js
+++ main.js
@@ -9,34 +9,51 @@ var scrollPos = 0;
 // adding scroll event
 window.addEventListener('scroll', function(){
 
-  // detects new state and compares it with the new one
-//   if ((document.body.getBoundingClientRect()).top > scrollPos)
-//   document.getElementById('adjective').setAttribute('data-scroll-direction', 'beautiful.');
+    // detects new state and compares it with the new one
+  //   if ((document.body.getBoundingClientRect()).top > scrollPos)
+  //   document.getElementById('adjective').setAttribute('data-scroll-direction', 'beautiful.');
+    
+
+
+
+  // else
+  //   document.getElementById('adjective').setAttribute('data-scroll-direction', 'dreadful.');
+  // // saves the new position for iteration.
+  // scrollPos = (document.body.getBoundingClientRect()).top;
+  // });
+
+  let adjective =document.querySelector('#adjective');
+  let scenario = document.querySelector('#scenario');
+  // let bg = document.querySelector();
+
+  // if ( (document.body.getBoundingClientRect()).top > scrollPos )
+  // document.getElementById('adjective').setAttribute('data-scroll-direction', 'beautiful.');
+
+
+
+  // else
+  // document.getElementById('adjective').setAttribute('data-scroll-direction', 'dreadful.');
+  // // saves the new position for iteration.
+  // scrollPos = (document.body.getBoundingClientRect()).top;
+
+
+  if ((document.body.getBoundingClientRect()).top > scrollPos) {
+    document.getElementById('adjective').setAttribute('data-scroll-direction', 'beautiful.');
+    document.getElementById('scenario').setAttribute('data-scroll-situation', 'know what & how to do with it');
   
-
-
-
-// else
-//   document.getElementById('adjective').setAttribute('data-scroll-direction', 'dreadful.');
-// // saves the new position for iteration.
-// scrollPos = (document.body.getBoundingClientRect()).top;
-// });
-
-let adjective =document.querySelector('#adjective');
-let scenario = document.querySelector('#scenario');
-
-if ((document.body.getBoundingClientRect()).top > scrollPos)
-document.getElementById('adjective').setAttribute('data-scroll-direction', 'beautiful.');
-
-
-
-
-
-else
-document.getElementById('adjective').setAttribute('data-scroll-direction', 'dreadful.');
-// saves the new position for iteration.
-scrollPos = (document.body.getBoundingClientRect()).top;
+  
+  
+  } else {
+    document.getElementById('adjective').setAttribute('data-scroll-direction', 'dreadful.');
+    document.getElementById('scenario').setAttribute('data-scroll-situation', 'stare at people\' code for 2 hrs but still dont know how to work that magic ');
+    // saves the new position for iteration.
+    scrollPos = (document.body.getBoundingClientRect()).top;
+  }
 });
+
+
+
+
 
 
 
